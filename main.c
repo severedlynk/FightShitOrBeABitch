e39c59f448a24e3c26abf956f362f33d476463ef
Quinn Johns <johnslachlan007@gmail.com>
2016-01-10 00:58:16 +1030

    Updated main.c 2

--- main.c
+++ main.c
@@ -3,8 +3,18 @@
 
 int main()
 {
+
+	char direction;
+
 	printf("Fight Shit Or Be A Bitch\n");
 	printf("by Quinn Johns\n");
+	printf("\n");
+	printf("\n");
+	printf("Choose a direction you wish to head:\n");
+	printf("A. \n");
+	printf("B. \n");
+	printf("C. \n");
+	scanf(" %c", &direction);
 
 	return 0;
 }
